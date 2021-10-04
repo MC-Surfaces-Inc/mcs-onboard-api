d97b85c05fe6f2b49e09ff4439b2303c1a2a03d6
Nicholas Simmons <nicksimmons53@gmail.com>
2021-10-04 13:57:50 -0500

    PUT Request on Program Route

--- routes/v1/programs.js
+++ routes/v1/programs.js
@@ -37,9 +37,9 @@ router.get("/info", (req, res) => {
 
 router.post("/info", (req, res) => {
   let program = mysql.raw(req.query.programName);
-  let sql = `insert into program_details_? set ? where clientId=? on duplicate key update set ?;`;
+  let sql = `insert into program_details_? set ? on duplicate key update set ?;`;
 
-  db.query(sql, [ program, req.body, req.query.clientId, req.body ], (err, data) => {
+  db.query(sql, [ program, req.body, req.body ], (err, data) => {
     if (err) throw err;
 
     res.json({ message: "Client Program Specs. Update Successfully." });
