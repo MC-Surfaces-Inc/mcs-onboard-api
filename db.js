3f52d6ab392da4ad465b6705ff828fecd08937a5
Nicholas Simmons <nicksimmons53@gmail.com>
2021-10-13 00:18:01 -0500

    To Production DB

--- db.js
+++ db.js
@@ -2,17 +2,22 @@ var mysql = require("mysql");
 
 var devDatabase = {
   connectionLimit: 20,
-  host: process.env.DB_HOST_DEV,
-  user: process.env.DB_USER_DEV,
-  password: process.env.DB_PASSWORD_DEV,
-  database: process.env.DB_NAME_DEV,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true
 };
 
 var prodDatabase = {
-
+  connectionLimit: 20,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME_PROD,
+  multipleStatements: true
 };
 
-var dbConnection = mysql.createPool(devDatabase);
+var dbConnection = mysql.createPool(prodDatabase);
 
 module.exports = dbConnection;
