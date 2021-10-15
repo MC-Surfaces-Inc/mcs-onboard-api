8261f5e1a24132fde8a4da00905b41fa34c8d453
Nicholas Simmons <nicksimmons53@gmail.com>
2021-10-14 23:28:24 -0500

    Finished Client Routes

--- routes/admin/pricing.js
+++ routes/admin/pricing.js
@@ -1,11 +1,11 @@
 var express = require("express");
-var router = express.Router( );
+var router = express.Router({ mergeParams: true });
 var mysql = require("mysql");
 
 var db = require("../../db");
 
 router.get("/", (req, res) => {
-  let sql = "select * from billing_parts;";
+  let sql = "select * from billing_parts where clientId=?;";
 
   db.query(sql, [ req.param.clientId ], (err, data) => {
     if (err) throw err;
