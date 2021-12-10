57bad392980df317333ea99fe062a2a007cd7d99
Nicholas Simmons <nicksimmons53@gmail.com>
2021-12-09 18:32:09 -0600

    Push to Sage Route Complete

--- routes/v1/clients.js
+++ routes/v1/clients.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router( );
 var mysql = require("mysql");
 var axios = require("axios");
+var _ = require("lodash");
+var { XMLParser } = require("fast-xml-parser");
 
 var db = require("../../db");
 
@@ -56,145 +58,284 @@ router.put("/:id", (req, res) => {
   });
 });
 
-router.get("/:id/sage-create", async(req, res) => {
-  let oktaDomain = process.env.OKTA_DOMAIN;
-  let oktaEncoding = process.env.OKTA_ENCODING;
+// Create a Client in Sage 
+router.get("/:id/sage-create", async (req, res) => {
   let mcsDomainAPI = process.env.MCS_API;
   let sql = "select * from clients join users on clients.userId=users.id where clients.id=?;";
   let sql2 = "select * from addresses where clientId=?;";
   let sql3 = "select * from contacts where clientId=?;";
   let sql4 = "select * from billing_parts where clientId=?;";
-  let sqlParams = [ req.params.id, req.params.id, req.params.id, req.params.id ];
-  let headers = {
-    'accept': "application/json",
-    'authorization': `Basic ${oktaEncoding}`,
-    'cache-control': "no-cache",
-    'content-type': "application/x-www-form-urlencoded"
-  };
+  let sql5 = "select * from programs where clientId=?;";
+  let sqlParams = [ req.params.id, req.params.id, req.params.id, req.params.id, req.params.id ];
+  let responses = [];
 
-  db.query(sql.concat(sql2, sql3, sql4), sqlParams, (err, data) => {
+  // Retrieve Valid Token from Okta to call 
+  const getValidToken = async( ) => {
+    let oktaDomain = process.env.OKTA_DOMAIN;
+    let oktaEncoding = process.env.OKTA_ENCODING;
+    let headers = {
+      'accept': "application/json",
+      'authorization': `Basic ${oktaEncoding}`,
+      'cache-control': "no-cache",
+      'content-type': "application/x-www-form-urlencoded"
+    };
+
+    let token;
+    await axios.post(oktaDomain, "grant_type=client_credentials&scope=OnBoard", { headers: headers })
+      .then((response) => {
+        token = response.data.access_token;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    
+    return token;
+  }
+
+  db.query(sql.concat(sql2, sql3, sql4, sql5), sqlParams, async (err, data) => {
     if (err) throw err;
 
     let client = {
       info: data[0][0],
       addresses: data[1],
       contacts: data[2],
-      billingParts: data[3]
+      billingParts: data[3],
+      programs: data[4][0]
     };
 
-    // let corpAddr = client.addresses.filter(address => address.type === "Corporate");
-    // let billingAddr = client.addresses.filter(address => address.type === "Billing");
-    // let shippingAddr = client.addresses.filter(address => address.type === "Shipping");
+    let corpAddr = client.addresses.filter(address => address.type === "Corporate");
+    let billingAddr = client.addresses.filter(address => address.type === "Billing");
+    let shippingAddr = client.addresses.filter(address => address.type === "Shipping");
 
-    // let sageClient = {
-    //   info: {
-    //     ShortName: client.info.shortName,
-    //     Name: client.info.name,
-    //     Addr1: corpAddr[0].address1,
-    //     Addr2: corpAddr[0].address2,
-    //     City: corpAddr[0].city,
-    //     State: corpAddr[0].state,
-    //     PostalCode: corpAddr[0].zip,
-    //     BillingAddr1: billingAddr[0].address1,
-    //     BillingAddr2: billingAddr[0].address2,
-    //     BillingCity: billingAddr[0].city,
-    //     BillingState: billingAddr[0].state,
-    //     BillingPostalCode: billingAddr[0].zip,
-    //     ShippingAddr1: shippingAddr[0].address1,
-    //     ShippingAddr2: shippingAddr[0].address2,
-    //     ShippingCity: shippingAddr[0].city,
-    //     ShippingState: shippingAddr[0].state,
-    //     ShippingPostalCode: shippingAddr[0].zip,
-    //     SalespersonRef: client.info.employeeNumber,
-    //     ManagerRef: client.info.arSpecialist,
-    //     ClientTypeRef: 1,
-    //     ClientStatusRef: 1
-    //   },
-    //   billingParts: [],
-    //   contacts: client.contacts.map(contact => {
-    //     return {
-    //       ContactName: contact.name,
-    //       JobTitle: contact.title,
-    //       Phone: contact.phone,
-    //       Email: contact.email
-    //     }
-    //   })
-    // };
+    let sageClient = {
+      info: {
+        ShortName: client.info.shortName,
+        Name: client.info.name,
+        Addr1: corpAddr[0].address1,
+        Addr2: corpAddr[0].address2,
+        City: corpAddr[0].city,
+        State: corpAddr[0].state,
+        PostalCode: corpAddr[0].zip,
+        BillingAddr1: billingAddr.length !== 0 ? billingAddr[0].address1 : "",
+        BillingAddr2: billingAddr.length !== 0 ? billingAddr[0].address2 : "",
+        BillingCity: billingAddr.length !== 0 ? billingAddr[0].city : "",
+        BillingState: billingAddr.length !== 0 ? billingAddr[0].state : "",
+        BillingPostalCode: billingAddr.length !== 0 ? billingAddr[0].zip : "",
+        ShippingAddr1: shippingAddr.length !== 0 ? shippingAddr[0].address1 : "",
+        ShippingAddr2: shippingAddr.length !== 0 ? shippingAddr[0].address2 : "",
+        ShippingCity: shippingAddr.length !== 0 ? shippingAddr[0].city : "",
+        ShippingState: shippingAddr.length !== 0 ? shippingAddr[0].state : "",
+        ShippingPostalCode: shippingAddr.length !== 0 ? shippingAddr[0].zip : "",
+        SalespersonRef: client.info.employeeNumber,
+        ManagerRef: client.info.arSpecialist,
+        ClientTypeRef: 1,
+        ClientStatusRef: 1
+      },
+      billingParts: [],
+      contacts: client.contacts.map(contact => {
+        return {
+          ContactName: contact.name,
+          JobTitle: contact.title,
+          Phone: contact.phone,
+          Email: contact.email
+        }
+      }),
+    };
+    
+    let token = await getValidToken( );
+    headers = {
+      'Authorization': 'Bearer ' + token,
+    };
 
-    // client.billingParts.forEach(row => {
-    //   if (row.program === "Carpet" || row.program === "Wood" || row.program === "LVP") {
-    //     if (row.programTable === "Miscellaneous" || row.programTable === "Carpet Pad") {
-    //       sageClient.billingParts.push({
-    //         description: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description}`,
-    //         unit: row.unit || "SqFt",
-    //         billingAmount: row.totalCost,
-    //       });
-
-    //       return;
-    //     }
-
-    //     sageClient.billingParts.push({
-    //       description: `${client.info.name} ${row.programTable} Level ${row.level}`,
-    //       unit: row.unit || "SqFt",
-    //       billingAmount: row.totalCost,
-    //     });
-    //   }
-
-    //   if (row.program === "Countertops") {
-    //     if (row.programTable === "Edges" || row.programTable === "Sinks" || row.programTable === "Miscellaneous") {
-    //       sageClient.billingParts.push({
-    //         description: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description || row.type}`,
-    //         unit: row.unit || "SqFt",
-    //         billingAmount: row.totalCost
-    //       });
-
-    //       return; 
-    //     }
-
-    //     sageClient.billingParts.push({
-    //       description: `${client.info.name} ${row.program} - ${row.programTable} ${row.type}`,
-    //       unit: row.unit || "SqFt",
-    //       billingAmount: row.totalCost
-    //     });
-    //   }
-
-    //   if (row.program === "Tile") {
-    //     if (row.programTable === "Patterns" || row.programTable === "Accents" || row.programTable === "Bath Accessories" || row.programTable === "Miscellaneous") {
-    //       sageClient.billingParts.push({
-    //         description: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description}`,
-    //         unit: row.unit || "SqFt",
-    //         billingAmount: row.totalCost
-    //       });
-
-    //       return;
-    //     }
-
-    //     sageClient.billingParts.push({
-    //       description: `${client.info.name} ${row.program} - ${row.programTable} Level ${row.level}`,
-    //       unit: row.unit || "SqFt",
-    //       billingAmount: row.totalCost
-    //     });
-    //   }
-    // });
-
-
-    axios.post(oktaDomain, "grant_type=client_credentials&scope=OnBoard", { headers: headers })
+    // Create Client
+    await axios.post(`${mcsDomainAPI}/Client`, sageClient, { headers: headers })
       .then((response) => {
-        let token = response.data.access_token;
-        axios.get(`${mcsDomainAPI}/api/Client/137`, { headers: { 'authorization': `Bearer ${token}`}})
-          .then((response) => {
-            console.log(response)
-          })
+        responses.push(response.status)
       })
       .catch((error) => {
         console.error(error);
       });
     
-    // res.json({
-    //   message: "Successfully Pushed to Sage.", 
-    //   data: sageClient,
-    //   status: 200 
-    // });
+    // Get Next Available Part Classes per Program
+    let partClasses;
+    let partClassAttrs;
+    await axios.get(`${mcsDomainAPI}/PartClass`, { headers: headers })
+      .then((response) => {
+        let parser = new XMLParser({
+          ignoreAttributes: false
+        });
+        let sageResponseJSON = parser.parse(response.data);
+        partClasses = _.get(sageResponseJSON, "api:MBXML.MBXMLMsgsRs.SQLRunRs.xml.rs:data.rs:insert.z:row");
+        partClassAttrs = _.get(sageResponseJSON, "api:MBXML.MBXMLMsgsRs.SQLRunRs.xml.s:Schema.s:ElementType.s:AttributeType");
+        
+        responses.push(response.status)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    
+    let lastTileProgram = _.last(partClasses.filter((partClass) => partClass["@_ObjectID"] > 200 && partClass["@_ObjectID"] < 1000));
+    let lastGraniteProgram = _.last(partClasses.filter((partClass) => partClass["@_ObjectID"] > 1001 && partClass["@_ObjectID"] < 1099));
+    let lastWoodProgram = _.last(partClasses.filter((partClass) => partClass["@_ObjectID"] > 4001 && partClass["@_ObjectID"] < 4100));
+    let lastCarpetProgram = _.last(partClasses.filter((partClass) => partClass["@_ObjectID"] > 7002 && partClass["@_ObjectID"] < 8000));
+
+    let countertopPartClass;
+    let tilePartClass;
+    let carpetPartClass;
+    let woodPartClass;
+
+    // Create Part Classes (multiple calls, one per program)
+    await Object.keys(client.programs).forEach(async (program) => {
+      if (programs[program] === 0 || program === "clientId") {
+        return;
+      }
+
+      let newPartClass;
+      if (program === "tile") {
+        newPartClass = parseInt(lastTileProgram["@_ObjectID"]) + 1;
+        tilePartClass = newPartClass;
+      }
+
+      if (program === "carpet") {
+        newPartClass = parseInt(lastCarpetProgram["@_ObjectID"]) + 1;
+        carpetPartClass = newPartClass;
+      }
+
+      if (program === "countertops") {
+        newPartClass = parseInt(lastGraniteProgram["@_ObjectID"]) + 1;
+        countertopPartClass = newPartClass;
+      }
+
+      if (program === "wood" || program === "vinyl") {
+        newPartClass = parseInt(lastWoodProgram["@_ObjectID"]) + 1;
+        woodPartClass = newPartClass;
+      }
+
+      let partClass = {
+        info: {
+          ObjectID: newPartClass,
+          Name: sageClient.info.Name,
+          IndentLevel: 2
+        }
+      };
+
+      await axios.post(`${mcsDomainAPI}/PartClass`, partClass, { headers: headers })
+        .then((response) => {
+          responses.push(response.status)
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+
+
+    client.billingParts.forEach(row => {
+      if (row.program === "Carpet") {
+        if (row.programTable === "Miscellaneous" || row.programTable === "Carpet Pad") {
+          sageClient.billingParts.push({
+            Desc: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description}`,
+            Unit: row.unit || "SqFt",
+            PartClassRef: carpetPartClass,
+            BillingAmount: row.totalCost,
+          });
+
+          return;
+        }
+
+        sageClient.billingParts.push({
+          Desc: `${client.info.name} ${row.programTable} Level ${row.level}`,
+          Unit: row.unit || "SqFt",
+          PartClassRef: carpetPartClass,
+          BillingAmount: row.totalCost,
+        });
+      }
+
+      if (row.program === "Wood" || row.program === "LVP") {
+        if (row.programTable === "Miscellaneous") {
+          sageClient.billingParts.push({
+            Desc: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description}`,
+            Unit: row.unit || "SqFt",
+            PartClassRef: woodPartClass,
+            BillingAmount: row.totalCost,
+          });
+
+          return;
+        }
+
+        sageClient.billingParts.push({
+          Desc: `${client.info.name} ${row.programTable} Level ${row.level}`,
+          Unit: row.unit || "SqFt",
+          PartClassRef: woodPartClass,
+          BillingAmount: row.totalCost,
+        });
+      }
+
+      // COUNTERTOPS
+      if (row.program === "Countertops") {
+        if (row.programTable === "Edges" || row.programTable === "Sinks" || row.programTable === "Miscellaneous") {
+          sageClient.billingParts.push({
+            Desc: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description || row.type}`,
+            Unit: row.unit || "SqFt",
+            PartClassRef: countertopPartClass,
+            BillingAmount: row.totalCost
+          });
+
+          return; 
+        }
+
+        sageClient.billingParts.push({
+          Desc: `${client.info.name} ${row.program} - ${row.programTable} ${row.type}`,
+          Unit: row.unit || "SqFt",
+          PartClassRef: countertopPartClass,
+          BillingAmount: row.totalCost
+        });
+      }
+
+      // TILE PART CLASS  
+      if (row.program === "Tile") {
+        if (row.programTable === "Patterns" || row.programTable === "Accents" || row.programTable === "Bath Accessories" || row.programTable === "Miscellaneous") {
+          sageClient.billingParts.push({
+            Desc: `${client.info.name} ${row.program} - ${row.programTable} - ${row.description}`,
+            Unit: row.unit || "SqFt",
+            PartClassRef: tilePartClass,
+            BillingAmount: row.totalCost
+          });
+
+          return;
+        }
+
+        sageClient.billingParts.push({
+          Desc: `${client.info.name} ${row.program} - ${row.programTable} Level ${row.level}`,
+          Unit: row.unit || "SqFt",
+          PartClassRef: tilePartClass,
+          BillingAmount: row.totalCost
+        });
+      }
+    });
+
+    // Create Parts (one call for all parts)
+    await axios.post(`${mcsDomainAPI}/Parts`, sageClient.billingParts, { headers: headers })
+      .then((response) => {
+        responses.push(response.status)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // Need to have accurate responses for all API calls
+    responses.forEach((response) => {
+      if (response < 200 || response > 299) {
+        res.send({
+          status: 500,
+          message: `There was an error pushing ${sageClient.info.Name} to Sage.`
+        });
+      }
+    });
+
+    res.send({
+      status: 200,
+      message: `${sageClient.info.Name} was successfully pushed to Sage.`
+    });
   });
 });
 
