50a4a101c984b8e652cc843826b44e02bf0a0d60
Nicholas Simmons <nicksimmons53@gmail.com>
2022-02-09 13:38:40 -0600

    Added Admin Routes

--- routes/admin/programs.js
+++ routes/admin/programs.js
@@ -24,13 +24,14 @@ router.get("/:id", (req, res) => {
   db.query(sql.concat(sql2, sql3, sql4, sql5), params, (err, data) => {
     if (err) throw err;
 
-    res.json({ 
-      programs: data,
-      cabinets: data[0],
-      carpet: data[1],
-      countertops: data[2],
-      tile: data[3],
-      woodVinyl: data[4],
+    res.json({
+      programs: {
+        cabinets: data[0],
+        carpet: data[1],
+        countertops: data[2],
+        tile: data[3],
+        woodVinyl: data[4],
+      }
     });
   });
 });
