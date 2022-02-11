de25d8775510b7b1a488aa84335b63f7be988ac1
Nicholas Simmons <nicksimmons53@gmail.com>
2022-02-11 13:42:10 -0600

    Admin Billing Parts Route

--- routes/admin/pricing.js
+++ routes/admin/pricing.js
@@ -5,27 +5,27 @@ var db = require("../../db");
 
 
 router.get("/:id", (req, res) => {
-  let carpetSQL = `select CONCAT_WS(" ", c.name, programTable, concat("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+  let carpetSQL = `select CONCAT_WS(" ", c.name, programTable, concat("Level ", level), description) as Description, program, programTable, unit as Unit, totalCost as BillingAmount
       from billing_parts
         join clients c on billing_parts.clientId = c.id
       where clientId=? and program="Carpet";
     `;
-  let countertopsSQL = `select CONCAT_WS(" ", c.name, program, programTable, type, description) as Description, program, unit as Unit, totalCost as BillingAmount
+  let countertopsSQL = `select CONCAT_WS(" ", c.name, program, programTable, type, description) as Description, program, programTable, unit as Unit, totalCost as BillingAmount
       from billing_parts
         join clients c on c.id = billing_parts.clientId
       where clientId=? and program="Countertops";
      `;
-  let tileSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("T", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+  let tileSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("T", level), description) as Description, program, programTable, unit as Unit, totalCost as BillingAmount
       from billing_parts
         join clients c on c.id = billing_parts.clientId
       where clientId=? and program="Tile";
     `;
-  let lvpSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+  let lvpSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, programTable, unit as Unit, totalCost as BillingAmount
       from billing_parts
         join clients c on c.id = billing_parts.clientId
       where clientId=? and program="LVP";
     `;
-  let woodSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+  let woodSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, programTable, unit as Unit, totalCost as BillingAmount
       from billing_parts
         join clients c on c.id = billing_parts.clientId
       where clientId=? and program="Wood";
