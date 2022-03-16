14248adcd22ebcddccd2d54eb26898d84c62c102
Nicholas Simmons <nicksimmons53@gmail.com>
2022-03-16 17:41:26 -0500

    Client Profile Data

--- routes/v1/clients.js
+++ routes/v1/clients.js
@@ -38,55 +38,53 @@ router.get("/", (req, res) => {
 });
 
 // Get all of a client's data
-router.get("/:id/compile-data", (req, res) => {
-  let sql = "select * from clients join users on clients.userId=users.id where clients.id=?;";
-  let sql2 = "select * from addresses where clientId=?;";
-  let sql3 = "select * from contacts where clientId=?;";
-  let sql4 = "select * from programs where clientId=?;";
-  let carpetSQL = `select CONCAT_WS(" ", c.name, programTable, concat("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
-      from billing_parts
-        join clients c on billing_parts.clientId = c.id
-      where clientId=? and program="Carpet";
-    `;
-  let countertopsSQL = `select CONCAT_WS(" ", c.name, program, programTable, type, description) as Description, program, unit as Unit, totalCost as BillingAmount
-      from billing_parts
-        join clients c on c.id = billing_parts.clientId
-      where clientId=? and program="Countertops";
-     `;
-  let tileSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("T", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
-      from billing_parts
-        join clients c on c.id = billing_parts.clientId
-      where clientId=? and program="Tile";
-    `;
-  let lvpSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
-      from billing_parts
-        join clients c on c.id = billing_parts.clientId
-      where clientId=? and program="LVP";
-    `;
-  let woodSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
-      from billing_parts
-        join clients c on c.id = billing_parts.clientId
-      where clientId=? and program="Wood";
-    `;
-  let sqlParams = Array(10).fill(req.params.id, 0, 10);
-
-  // Query Application DB for Client Data
-  db.query(sql.concat(sql2, sql3, sql4, carpetSQL, countertopsSQL, tileSQL, lvpSQL, woodSQL), sqlParams, async (err, data) => {
-    if (err) throw err;
-
-    let client = {
-      info: data[0][0],
-      addresses: data[1],
-      contacts: data[2],
-      programs: data[3][0],
-      billingParts: data[4].concat(data[5], data[6], data[7], data[8])
-    };
-
-    console.log(client.info)
-
-    res.send(client);
-  });
-});
+// router.get("/:id/compile-data", (req, res) => {
+//   let sql = "select * from clients join users on clients.userId=users.id where clients.id=?;";
+//   let sql2 = "select * from addresses where clientId=?;";
+//   let sql3 = "select * from contacts where clientId=?;";
+//   let sql4 = "select * from programs where clientId=?;";
+//   let carpetSQL = `select CONCAT_WS(" ", c.name, programTable, concat("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+//       from billing_parts
+//         join clients c on billing_parts.clientId = c.id
+//       where clientId=? and program="Carpet";
+//     `;
+//   let countertopsSQL = `select CONCAT_WS(" ", c.name, program, programTable, type, description) as Description, program, unit as Unit, totalCost as BillingAmount
+//       from billing_parts
+//         join clients c on c.id = billing_parts.clientId
+//       where clientId=? and program="Countertops";
+//      `;
+//   let tileSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("T", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+//       from billing_parts
+//         join clients c on c.id = billing_parts.clientId
+//       where clientId=? and program="Tile";
+//     `;
+//   let lvpSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+//       from billing_parts
+//         join clients c on c.id = billing_parts.clientId
+//       where clientId=? and program="LVP";
+//     `;
+//   let woodSQL = `select CONCAT_WS(" ", c.name, programTable, CONCAT("Level ", level), description) as Description, program, unit as Unit, totalCost as BillingAmount
+//       from billing_parts
+//         join clients c on c.id = billing_parts.clientId
+//       where clientId=? and program="Wood";
+//     `;
+//   let sqlParams = Array(10).fill(req.params.id, 0, 10);
+//
+//   // Query Application DB for Client Data
+//   db.query(sql.concat(sql2, sql3, sql4, carpetSQL, countertopsSQL, tileSQL, lvpSQL, woodSQL), sqlParams, async (err, data) => {
+//     if (err) throw err;
+//
+//     let client = {
+//       info: data[0][0],
+//       addresses: data[1],
+//       contacts: data[2],
+//       programs: data[3][0],
+//       billingParts: data[4].concat(data[5], data[6], data[7], data[8])
+//     };
+//
+//     res.send(client);
+//   });
+// });
 
 // router.get("/:id/profile-data", (req, res) => {
 //   let sql = `
