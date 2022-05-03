f7bacb374ab6b1a7957c624b647333922d65f141
Nicholas Simmons <nicksimmons53@gmail.com>
2022-05-03 10:01:27 -0500

    Admin Client Route

--- routes/admin/clients.js
+++ routes/admin/clients.js
@@ -10,13 +10,29 @@ router.use("/details", require("./details"));
 router.use("/pricing", require("./pricing"));
 router.use("/programs", require("./programs"));
 
-router.get("/:id", (req, res) => {
-  let sql = "select * from clients where id=?;";
+router.get("/:id/profile-data", (req, res) => {
+  let sql = `
+    select name, shortName, territory from clients where id=?;
+    select type, CONCAT_WS(" ", address1, address2) address, city, state, zip from addresses where clientId=?;
+    select name, title, phone, email from contacts where clientId=?;
+    select lisak "Lisa Kallus", edythc "Edyth Cruz", kimn "Kim Conover" from approvals where clientId=?;
+    select cabinets "Cabinets", carpet "Carpet", countertops "Countertops", tile "Tile", wood "Wood", vinyl "Vinyl" from programs where clientId=?;
+    select status current from status where clientId=?;
+    `
+  ;
+  let params = Array(6).fill(req.params.id);
 
-  db.query(sql, [ req.params.id ], (err, data) => {
+  db.query(sql, params, (err, data) => {
     if (err) throw err;
 
-    res.json({ info: data[0] });
+    res.json({
+      basicInfo: data[0][0],
+      addresses: data[1],
+      contacts: data[2],
+      approvals: data[3][0],
+      programs: data[4][0],
+      status: data[5][0]
+    });
   });
 });
 
