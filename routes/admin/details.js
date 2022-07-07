baed8daf5fcf5a8b2766bb2bfe3b755e04115993
Nicholas Simmons <nicksimmons53@gmail.com>
2022-07-07 11:17:00 -0500

    Admin Route

--- routes/admin/details.js
+++ routes/admin/details.js
@@ -5,7 +5,10 @@ var mysql = require("mysql");
 var db = require("../../db");
 
 router.get("/", (req, res) => {
-  let sql = "select * from accounting_details where clientId=?; select * from expediting_details where clientId=?;"
+  let sql = `
+    select paymentFrequency, autopay, invoiceEmailAddress, paymentType, paymentPortal, paymentURL, poRequired, poInvoiceRequired, approvalsRequired, contractAttached, contactName, contactPhone, contactEmail, notes from accounting_details where clientId=?; 
+    select vendorPortal, vendorPortalURL, portalAccountCreated, portalUsername, portalPassword, jobReleaseMethod, poErrorHandling, estimatedHomes, estimatedStartDate, inHouseProgram, notes from expediting_details where clientId=?;
+  `;
 
   db(req.baseUrl).query(sql, [ req.query.clientId, req.query.clientId ], (err, data) => {
     if (err) throw err;
