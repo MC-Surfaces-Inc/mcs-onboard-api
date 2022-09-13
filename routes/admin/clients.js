c3b52a0cf81722089ea983eaaaee9ff9e490cf5e
Nick Simmons <nicksimmons53@gmail.com>
2022-09-12 21:54:58 -0500

    Added Field to Query

--- routes/admin/clients.js
+++ routes/admin/clients.js
@@ -15,9 +15,9 @@ router.get("/:id/profile-data", (req, res) => {
     select clients.id as clientId, name, shortName, territory, concat_ws(" ", firstName, lastName) as salesRep, email as salesRepEmail, phone as salesRepPhone, sageEmployeeNumber, sageUserId from clients join users u on clients.userId=u.id where clients.id=?;
     select type, CONCAT_WS(" ", address1, address2) address, city, state, zip from addresses where clientId=?;
     select name, title, phone, email from contacts where clientId=?;
-    select lisak "Lisa Kallus", edythc "Edyth Cruz", kimn "Kim Conover" from approvals where clientId=?;
+    select lisak "Lisa Kallus", edythc "Edyth Cruz", kimn "Kim Conover", lastSubmittedAt from approvals where clientId=?;
     select cabinets "Cabinets", carpet "Carpet", countertops "Countertops", tile "Tile", wood "Wood", vinyl "Vinyl" from programs where clientId=?;
-    select status current, lastSubmittedAt from status where clientId=?;
+    select status current from status where clientId=?;
     select cabinets, carpet, countertops, tile, wood, vinyl from programs where clientId=?;
     `
   ;
