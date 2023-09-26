5789b3de9534a78443205c682a1541ac0fa0130c
Nick Simmons <nicksimmons53@gmail.com>
2023-09-26 11:14:02 -0500

    XML Converter

--- routes/admin/sage.js
+++ routes/admin/sage.js
@@ -227,7 +227,7 @@ router.get("/clients/:id", (req, res) => {
       .then((response) => {
         let jsonResponse = convert.xml2json(response.data, { compact: true, spaces: 4 });
         let client = jsonResponse["ClientQryRs"];
-        res.send({ client: client });
+        res.send(jsonResponse);
       })
       .catch((err) => {
         if (err) {
