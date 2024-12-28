7806052889ca87b60e0171ab125b232e9d3131db
Nick Simmons <nicksimmons53@gmail.com>
2024-12-28 16:12:51 -0600

    Upload SP File

--- routes/v1/sharepoint.js
+++ routes/v1/sharepoint.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const axios = require("axios");
+const multer = require("multer");
+const FormData = require("form-data");
 
 const db = require("../../db");
 const logger = require("../common/Logging/logger");
+const fs = require("node:fs");
+
+const upload = multer();
 
 router.get("/folder", async (req, res) => {
   await axios.get(`${process.env.MCS_MICROSOFT_API_URL}/sharepoint/folder?id=${req.query.id}`)
@@ -17,6 +22,27 @@ router.post("/folder", async (req, res) => {
     await axios.post(`${process.env.MCS_MICROSOFT_API_URL}/sharepoint/folder?parentId=${req.query.parentId}&folder=${req.query.folder}`)
       .then((response) => {
         res.status(200).json({ message: "Successfully created folder" });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/file", upload.single('file'), async (req, res) => {
+  const formData = new FormData();
+  const fileBuffer = Buffer.from(req.file.buffer);
+
+  formData.append("file", fileBuffer, req.file.originalname);
+
+  try {
+    await axios.post(`${process.env.MCS_MICROSOFT_API_URL}/sharepoint/file?parentId=${req.query.parentId}`, formData, {
+      headers: formData.getHeaders()
+    })
+      .then((response) => {
+        res.status(200).json({ message: "Successfully uploaded file" });
       })
       .catch((error) => {
         console.log(error);
