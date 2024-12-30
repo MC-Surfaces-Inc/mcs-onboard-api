c6a1f2b757ebc862f6048f81c8f13af6a179c089
Nick Simmons <nicksimmons53@gmail.com>
2024-12-30 11:46:47 -0600

    Internal folder creation

--- routes/v1/sharepoint.js
+++ routes/v1/sharepoint.js
@@ -9,6 +9,30 @@ const logger = require("../common/Logging/logger");
 
 const upload = multer();
 
+router.post("/folder/internal", async (req, res) => {
+  let sql = "insert into folder set ?";
+  let body = {
+    ...req.body,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+
+  db(req.baseUrl).query(sql, body, (err, data) => {
+    if (err) {
+      logger.log({
+        level: "error",
+        message: err,
+        protocol: req.protocol,
+        route: req.originalUrl,
+        timestamp: new Date()
+      });
+      throw err;
+    }
+
+    res.status(200).json({ message: "Folder Successfully Created." });
+  });
+})
+
 router.get("/folder", async (req, res) => {
   await axios.get(`${process.env.MCS_MICROSOFT_API_URL}/sharepoint/folder?id=${req.query.id}`)
     .then((response) => {
